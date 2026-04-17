a36c4e277aa82e01c6c593526bf6423b98d59887
IAFahim <ishtiaq.fahim@northsouth.edu>
2026-04-17 10:03:12 +0600

    code

---
+++ BovineLabs.Timeline.Essence/AssemblyInfo.cs
@@ -0,0 +1,9 @@
+// <copyright file="AssemblyInfo.cs" company="iafahim">
+// Copyright (c) iafahim. All rights reserved.
+// </copyright>
+
+using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("BovineLabs.Timeline.Essence.Debug")]
+[assembly: InternalsVisibleTo("BovineLabs.Timeline.Essence.Editor")]
+[assembly: InternalsVisibleTo("BovineLabs.Timeline.Essence.Tests")]
